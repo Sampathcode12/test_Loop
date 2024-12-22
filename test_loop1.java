48be1998eeb3995d3ee0aed6682192f1e6193aae
Sampathcode12 <lsampathlk4@gmail.com>
2024-12-22 21:25:45 +0530

    first loop

---
+++ test_loop1.java
@@ -0,0 +1,8 @@
+public class test_loop1 {
+    public static void main(String[] args) {
+        // A simple for loop that prints numbers from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
